10b3cdb4160dfa38b5830321b80c4d5e1ee30da4
rmcapunpon <rmcapunpon@nu-laguna.edu.ph>
2026-03-03 20:06:25 +0800

    Update ObjectDetailsPrinter to activate student behavior
    Removed toString usage and added a method call to raiseHand.

--- classes-and-objects-lab/ObjectDetailsPrinter.java
+++ classes-and-objects-lab/ObjectDetailsPrinter.java
@@ -1,5 +1,6 @@
 public class ObjectDetailsPrinter {
     public static void main(String[] args) {
+        // Ramon's Object
         // This is the object instance created from the Student class
         Student s = new Student("Alice", 20, 3.85);
         System.out.println("Student details:");
@@ -7,10 +8,21 @@ public class ObjectDetailsPrinter {
         System.out.println("Age: " + s.getAge());
         System.out.println("GPA: " + s.getGpa());
 
-        // using toString
-        System.out.println("\nUsing toString(): " + s);
+        // activate behavior
+        s.raiseHand(2);
     }
 }
 
 // javac classes-and-objects-lab/Student.java classes-and-objects-lab/ObjectDetailsPrinter.java
 // java -cp classes-and-objects-lab ObjectDetailsPrinter
+
+/*
+* Tasks:
+* 1. Create your own class. e.g. Dog, Professor, SecurityGuard
+  2. Your class should have at least 3 attributes
+  3. Your class should have at least 2 behaviors.
+      - at least one behavior must have a parameter
+      - at least one behavior must have no parameters
+  4. Create an object using a Class created by one of your teammates. (A Class cannot be reused.)
+  5. Print out the attributes, and trigger the behaviors of the object you created.
+*/
