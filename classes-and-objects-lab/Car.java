d175f5dc4d84cfcf6b425b8c396db4e218342df1
Bien Manuel P. Badosa <bienmanuelbadosa@gmail.com>
2026-03-09 02:44:22 +0000

    myclass

--- classes-and-objects-lab/Car.java
+++ classes-and-objects-lab/Car.java
@@ -1,5 +1,5 @@
 /*
-* This class represents a student with a name, age, and GPA.
+* This class represents a car with its brand name and model along with its engine status.
 * Created by Bien Manuel Badosa
 */
 
