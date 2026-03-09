6fde8427fb0a3642a03316cb0c8c42b7c6ebc323
Julliana Nepomuceno <nepomucenojulliana@gmail.com>
2026-03-09 10:36:09 +0800

    Add overloaded constructor and display methods

--- classes-and-objects-lab/cellphone.java
+++ classes-and-objects-lab/cellphone.java
@@ -23,4 +23,26 @@ public class cellphone {
         this.storage = storage;
         totalPhonesSold++;
     }
+
+    // Overloaded constructor
+    cellphone(String brand, double price){
+        this.brand = brand;
+        this.price = price;
+        this.storage = 128; // phone base model storage
+    }
+
+    // Behavior without parameters
+    void display() {
+        System.out.println("Brand: " + brand + "| Price: " + price + "| Storage: " + storage);
+    }
+
+    // Behavior with parameters
+    void updateBrand(String newBrand){
+        brand = newBrand;
+        System.out.println("The phone brand was updated to: " + brand);
+    }
+    // Static method
+    static void displayTotalPhonesSold() {
+        System.out.println("Total Phones Sold: " + totalPhonesSold);
+    }
 }
