bcec4e189f3c0055c02c01942d2d05173ef2b006
Lynette Grace L. Galindon <galindonlynette@gmail.com>
2026-03-09 02:47:18 +0000

    bili  napo kayo

---
+++ classes-and-objects-lab/Seller.java
@@ -0,0 +1,71 @@
+public class Seller {
+ 
+    String name;
+    String product;
+    double price;
+    double totalSales;
+ 
+    static int totalSellers = 0;
+ 
+    // Default Constructor
+    Seller() {
+        this.name = "Unknown";
+        this.product = "Unknown";
+        this.price = 0.0;
+        this.totalSales = 0;
+        totalSellers++;
+    }
+ 
+    // Parameterized Constructor
+    Seller(String name, String product, double price) {
+        this.name = name;
+        this.product = product;
+        this.price = price;
+        this.totalSales = 0;
+        totalSellers++;
+    }
+ 
+    // Show seller info
+    void showSeller() {
+        System.out.println("Seller: " + name);
+        System.out.println("Product: " + product);
+        System.out.println("Price: ₱" + price);
+        System.out.println();
+    }
+ 
+    // Sell product
+    void sell(int quantity) {
+        double sale = quantity * price;
+        totalSales += sale;
+ 
+        System.out.println(name + " sold " + quantity + " " + product);
+        System.out.println("Sale: ₱" + sale);
+        System.out.println("Total Sales: ₱" + totalSales);
+        System.out.println();
+    }
+ 
+    // Static method
+    static void showTotal() {
+        System.out.println("Total Sellers Created: " + totalSellers);
+    }
+ 
+    // Main method
+    public static void main(String[] args) {
+ 
+        // Using default constructor
+        Seller s0 = new Seller();
+        s0.showSeller();
+ 
+        // Using parameterized constructor
+        Seller s1 = new Seller("Max",  "Dubai Chewy Cookie", 135);
+        Seller s2 = new Seller("Ralph",  "Chocolate Xiao Long Bao", 129);
+ 
+        s1.showSeller();
+        s2.showSeller();
+ 
+        s1.sell(12);
+        s2.sell(24);
+ 
+        Seller.showTotal();
+    }
+}
