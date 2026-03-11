c1ab70ee18aea8f8014e1b5c820b53bd510ee7dc
Jherrymei <jherraran01@gmail.com>
2026-03-11 13:37:57 +0000

    Arandela Class and Objects

--- classes-and-objects-lab/ObjectDetailsPrinter.java
+++ classes-and-objects-lab/ObjectDetailsPrinter.java
@@ -300,14 +300,29 @@ public class ObjectDetailsPrinter {
     //behavior with parameter
     pb2.updateCategory("Main Stream");
 
-
     //static method
     Product_Bautista.displayTotalProducts();
-
     System.out.println();
 
-    
+    // Object Created by Arandela, class created by alonde
+    // Default
+    Clothing_Alonde c1 = new Clothing_Alonde();
+    c1.displayClothing();
+    System.out.println();
 
+    // Parameter
+    Clothing_Alonde c2 = new Clothing_Alonde("T-shirt", 250.0, true);
+    c2.displayClothing();
+    System.out.println();
+
+    //Overloaded
+    Clothing_Alonde c3 = new Clothing_Alonde("Jacket", 1999.9);
+    c3.displayClothing();
+    System.out.println();
+
+    c3.updateType("Hoodie");
+
+    System.out.println();
 
     }
 
