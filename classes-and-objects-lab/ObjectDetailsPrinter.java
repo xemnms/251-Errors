d42230aec06c32bcbbdafb27b85dfe8513cfeeeb
Kayle Crivan K. Alonde <alondekayle7@gmail.com>
2026-03-12 01:58:10 +0000

    Alonde Object Creation.

--- classes-and-objects-lab/ObjectDetailsPrinter.java
+++ classes-and-objects-lab/ObjectDetailsPrinter.java
@@ -324,7 +324,23 @@ public class ObjectDetailsPrinter {
 
     System.out.println();
 
-    
+    // Object created by Alonde, Class created by Arandela
+    // Object without parameter
+    Arandela_Order order1 = new Arandela_Order();
+    order1.showOrder();
+    order1.addOrder(0);
+
+    // Object with parameter
+    Arandela_Order order2 = new Arandela_Order();
+    order2.showOrder("001", "Empanada", 5, 24.5);
+    order2.addOrder(3);
+
+    // Object overloaded constructor
+    Arandela_Order order3 = new Arandela_Order("002", "Dubai Chewy Cookie");
+    order3.showOrder();
+    order3.addOrder(5);
+
+    Arandela_Order.showTotalOrders();
     }
 
 }
