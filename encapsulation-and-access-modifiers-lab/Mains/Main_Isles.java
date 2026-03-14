070c269b09234e3b989e56533e74777ba69a2b2d
Dan Marvin M, Isles <zdanmarvin@gmail.com>
2026-03-14 13:35:26 +0800

    Create Main_Isles.java

---
+++ encapsulation-and-access-modifiers-lab/Mains/Main_Isles.java
@@ -0,0 +1,66 @@
+public class printer {
+    public static void main(String[] args) {
+    	//objects created by dan isles, class made by kyla rodenas
+        System.out.println("=======LRCBook Demo=======\n");
+        //creates first book using default constructor
+        LRCBookRodenas book1 = new LRCBookRodenas();
+        book1.setBookId(-15);
+        book1.setTitle("");
+        book1.setPages(250);
+        System.out.println();
+
+        //initializes getters to print book details out
+        book1.getBookDetails();
+        book1.borrowBook();
+        book1.getBookDetails();
+
+        System.out.println();
+
+        //creates second book using parameterized constructor
+        LRCBookRodenas book2 = new LRCBookRodenas(102, "", 320);
+
+        book2.getBookDetails();
+        book2.borrowBook();
+        book2.returnBook();
+        book2.getBookDetails();
+
+        System.out.println();
+
+        //shows total books borrowed and created
+        LRCBookRodenas.getTotalBooks();
+        LRCBookRodenas.getTotalBorrowedBooks();
+        
+    	//objects created by dan isles, class made by clisha batangan
+        System.out.println("\n=======Playlist_Batangan Demo=======\n");
+        //creates first playlist using default constructor
+        Playlist_Batangan playlist1 = new Playlist_Batangan();
+        playlist1.setPlaylistName("Chill Vibes");
+        playlist1.setGenre("Lo-Fi");
+        playlist1.setNumberOfSongs(-5);
+        playlist1.setDurationMinutes(25);
+
+        playlist1.displayPlaylist();
+
+        //add and remove songs
+        playlist1.addSong(1);
+        playlist1.removeSong(3);
+
+        System.out.println("After adding/removing songs:");
+        playlist1.displayPlaylist();
+        System.out.println();
+
+        //creates second playlist using parameterized constructor
+        Playlist_Batangan playlist2 = new Playlist_Batangan("Workout Hits", "Pop", 10, 40);
+        playlist2.displayPlaylist();
+
+        //adds a song
+        playlist2.addSong(-12);
+        System.out.println("After adding a song:");
+        playlist2.displayPlaylist();
+
+        System.out.println();
+
+        //shows total playlists created
+        System.out.println("Total playlists created: " + Playlist_Batangan.getTotalPlaylists());
+    }
+}
