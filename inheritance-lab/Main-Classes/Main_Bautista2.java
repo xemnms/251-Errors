73c659928052ad35f10a9c55c7a279b8f0a48b22
Lei <leibautista1417@gmail.com>
2026-03-16 08:36:35 +0000

    Updating 4.0

--- inheritance-lab/Main-Classes/Main_Bautista2.java
+++ inheritance-lab/Main-Classes/Main_Bautista2.java
@@ -7,7 +7,7 @@
 
 // Main class created by Bautista
 
-public class MainInheritance_Bautista {
+public class Main_Bautista2 {
     public static void main(String[] args) {
 
         System.out.println("==========================================");
