99c38be4c0f8c6ab47b4260e8511a15f63a751cb
Lei <leibautista1417@gmail.com>
2026-03-16 04:38:04 +0000

    Add main, subclass, and updates personal files.

--- inheritance-lab/MainInheritance_Bautista.java
+++ inheritance-lab/MainInheritance_Bautista.java
@@ -1,4 +1,4 @@
-public class Main_Bautista {
+public class MainInheritance_Bautista {
     public static void main(String[] args) {
 
         System.out.println("==========================================");
