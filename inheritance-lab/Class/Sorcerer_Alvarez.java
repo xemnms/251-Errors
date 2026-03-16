f054ae1ccefd919ea2ace00d621fdfcd5dd3eea8
Angelo Hayden Alvarez <hayc1en.alvarez@gmail.com>
2026-03-16 02:03:44 +0000

    updated parent class alvarez

--- inheritance-lab/Class/Sorcerer_Alvarez.java
+++ inheritance-lab/Class/Sorcerer_Alvarez.java
@@ -4,13 +4,13 @@ public class Sorcerer_Alvarez {
     private static final String Class_ID = "Sorcerer Class Created by Alvarez";
 
     // Attributes
-    private String jujustuGrade;
+    private String jujutsuGrade;
     private String cursedTechnique;
     private double cursedEnergyConsumption;
 
     // Constructor
-    public Sorcerer(String jujustuGrade, String cursedTechnique, double cursedEnergyConsumption) {
-        this.jujustuGrade = jujustuGrade;
+    public Sorcerer(String jujutsuGrade, String cursedTechnique, double cursedEnergyConsumption) {
+        this.jujutsuGrade = jujutsuGrade;
         this.cursedTechnique = cursedTechnique;
         this.cursedEnergyConsumption = cursedEnergyConsumption;
     }
@@ -24,13 +24,13 @@ public class Sorcerer_Alvarez {
     // Behavior 2
     public void displayInfo() {
         System.out.println(Class_ID);
-        System.out.println("Grade: " + jujustuGrade);
+        System.out.println("Grade: " + jujutsuGrade);
         System.out.println("Technique: " + cursedTechnique);
     }
 
     // method that can be overidden later
     public void attack() {
-        System.out.printl("The Sorcerer attacks using Cursed Energy!");
+        System.out.println("The Sorcerer attacks using Cursed Energy!");
     }
 
     // Getters
