f6038cc6be763853089fc1f7440696f0887bbf4f
Clisha Rae <clishabatangan28@gmail.com>
2026-03-16 16:41:08 +0800

    Update Main_Batangan.java

--- inheritance-lab/Main-Classes/Main_Batangan.java
+++ inheritance-lab/Main-Classes/Main_Batangan.java
@@ -1,13 +1,16 @@
+/*
+ * Main created by Clisha Rae Batangan
+ */
 public class Main_Batangan {
- public static void main(String[] args) {
-
-// Runtime Polymorphism
- Device_Rodenas device = new Smartphone_Batangan("Samsung", "Galaxy S23", "Android");
-
- device.powerOn();
- device.showDeviceInfo();
- device.crashDevice(); // This will call the overridden method
- device.powerOff();
-
- }
+    public static void main(String[] args) {
+ 
+        // Runtime Polymorphism
+        Device_Rodenas device = new Smartphone_Batangan("Samsung", "Galaxy S23", "Android");
+ 
+        device.powerOn();
+        device.showDeviceInfo();
+        device.crashDevice();  // This will call the overridden method
+        device.powerOff();
+ 
+    }
 }
