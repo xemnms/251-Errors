c8671c1c595c570a653550c2f7293ae65022b466
Andrew Dizon <vicandrewdizon24@gmail.com>
2026-03-19 09:30:41 +0000

    added child class

---
+++ inheritance-lab/Child-Class/F1Prodigy_Dizon.java
@@ -0,0 +1,26 @@
+public class F1Prodigy extends Formula1 {
+    private boolean drsEnabled;
+
+    public F1Prodigy(String team, int speed, String driver, boolean drsEnabled) {
+        // parent constructor
+        super(team, speed, driver);
+        this.drsEnabled = drsEnabled;
+    }
+
+    // Add new behavior unique to the child class
+    public void toggleDRS() {
+        drsEnabled = !drsEnabled;
+        String status = drsEnabled ? "Enabled" : "Disabled";
+        System.out.println("DRS is now " + status + " for " + driver);
+    }
+
+    //  Override a parent method
+    @Override
+    public void race() {
+        if (drsEnabled) {
+            System.out.println(driver + " is pushing " + team + " to the limit with DRS open!");
+        } else {
+            System.out.println(team + " is maintaining tactical pace on the track.");
+        }
+    }
+}
