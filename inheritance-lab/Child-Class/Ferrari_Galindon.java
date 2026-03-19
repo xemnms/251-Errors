3608a6596776ea2e58a31d208fea8909c342ec32
Lynette Grace L. Galindon <galindonlynette@gmail.com>
2026-03-19 11:52:58 +0000

    galindon child class

---
+++ inheritance-lab/Child-Class/Ferrari_Galindon.java
@@ -0,0 +1,41 @@
+//parent class by arandela, child class by galindon
+//first child class!! represents a sports car
+
+class FerrariCar extends Formula1_Arandela {
+
+    // Constructor
+    public FerrariCar(String team, int speed, String driver) {
+        super(team, speed, driver);
+    }
+
+    //  New behavior
+    public void activateTurbo() {
+        System.out.println(team + " activates turbo boost!");
+    }
+
+    // Override parent method
+    @Override
+    public void race() {
+        System.out.println(team + " Ferrari car speeds aggressively on the track!");
+    }
+}
+
+//second child class!! represents a luxury sports car
+class BugattiCar extends Formula1_Arandela {
+
+    // Constructor
+    public BugattiCar(String team, int speed, String driver) {
+        super(team, speed, driver);
+    }
+
+    // New behavior
+    public void activatePremiumSoundSystem() {
+        System.out.println(team + " activates premium sound system!");
+    }
+
+    // Override parent method
+    @Override
+    public void race() {
+        System.out.println(team + " luxury car cruises smoothly on the track!");
+    }
+}
