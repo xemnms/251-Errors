065a4a960ddc13416d7227a8409e038e6c15849b
Julliana Nepomuceno <nepomucenojulliana@gmail.com>
2026-03-19 15:50:16 +0000

    Galindon Bug Fix

--- inheritance-lab/Main-Classes/Main_Galindon2.java
+++ inheritance-lab/Main-Classes/Main_Galindon2.java
@@ -3,13 +3,13 @@ public class Main_Galindon2 {
     public static void main(String[] args) {
 
         // Dynamic binding example
-        MusicEra_Alonde era = new PopEra("1989", 2014, 13);
+        MusicEra_Alonde era = new PopEra_Galindon("1989", 2014, 13);
 
         era.playEra();       // Calls overridden method
         era.displayInfo();  // Calls parent method
 
         // Access child-specific method
-        PopEra pop = new PopEra("Lover", 2019, 18);
+        PopEra_Galindon pop = new PopEra_Galindon("Lover", 2019, 18);
         pop.performConcert();
 
         System.out.println("Total Eras Created: " + MusicEra_Alonde.getTotalEras());
