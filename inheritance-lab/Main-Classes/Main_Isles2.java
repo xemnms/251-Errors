b95f034de4db826c6bfc4642334f7f3397d04072
Dan Marvin M, Isles <zdanmarvin@gmail.com>
2026-03-19 21:22:29 +0800

    Update Main_Isles2.java w/ answered questions in main

--- inheritance-lab/Main-Classes/Main_Isles2.java
+++ inheritance-lab/Main-Classes/Main_Isles2.java
@@ -1,4 +1,11 @@
 //main class!! where the outputs are done
+
+//1 the IS-A relationship in the program is that Laptop_Isles is a Device_Rodenas and Phone_Isles is also a Device_Rodenas, meaning both subclasses inherit from the parent class.
+//2 the method that was overridden in the program is crashDevice(), as both Laptop_Isles and Phone_Isles provide their own implementation of this method instead of using the one from the parent class.
+//3 during dynamic binding, the program determines at runtime which version of the overridden method to execute, so even if the reference type is Device_Rodenas, the actual method called depends on whether the object is a Laptop_Isles or a Phone_Isles.
+//4 The methods inherited from the parent class include powerOn() and any other common methods defined in Device_Rodenas that were not overridden by the subclasses.
+//5 The subclasses introduced new behavior by adding unique methods, where Laptop_Isles has runProgram() and Phone_Isles has takePhoto(), which are not present in the parent class.
+
 public class Main_Isles2 {
     public static void main(String[] args) {
         //creating laptop object
