67193b578502707b20846c8205473562c13e8001
Bien Manuel P. Badosa <bienmanuelbadosa@gmail.com>
2026-03-20 08:58:35 +0000

    Updating to develop team 3

--- composition-lab/DemoFolder/CompositionDemoCostiniano.java
+++ composition-lab/DemoFolder/CompositionDemoCostiniano.java
@@ -1,5 +1,6 @@
 
 package DemoFolder;
+
 //this demo class was created by Costiniano
 import ComponentClasses.Pistol_Badosa;
 import ComponentClasses.SchedulePlanner_Rodenas;
@@ -8,25 +9,23 @@ import CompositeClasses.AgentMission_Costiniano;
 public class CompositionDemoCostiniano {
 
     public static void main(String[] args) {
-        
-        //creates a pistol object
+
+        // creates a pistol object
         Pistol_Badosa pistol = new Pistol_Badosa("Glock", 2, 15);
-        
-        //creates a schedule object
-        SchedulePlanner_Rodenas schedule =
-                new SchedulePlanner_Rodenas("Monday", "10:00", "Recon Mission");
-        
-        //combines both objects into one mission
-        AgentMission_Costiniano mission =
-                new AgentMission_Costiniano(pistol, schedule);
-        
-        //starts the mission (uses pistol and schedule)
+
+        // creates a schedule object
+        SchedulePlanner_Rodenas schedule = new SchedulePlanner_Rodenas("Monday", "10:00", "Recon Mission");
+
+        // combines both objects into one mission
+        AgentMission_Costiniano mission = new AgentMission_Costiniano(pistol, schedule);
+
+        // starts the mission (uses pistol and schedule)
         mission.StartMission();
-        
-        //reloads the pistol
+
+        // reloads the pistol
         mission.reloadWeapon();
-        
-        //changes the mission task
+
+        // changes the mission task
         mission.updateMission("Stealth Operation");
     }
 }
