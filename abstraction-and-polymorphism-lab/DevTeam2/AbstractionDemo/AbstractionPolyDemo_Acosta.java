2f1bd35102107ad57d38100c75b5af8379663846
Lorcey Acosta <acostajl@students.nu-laguna.edu.ph>
2026-03-25 20:50:22 +0800

    Update AbstractionPolyDemo_Acosta.java

--- abstraction-and-polymorphism-lab/DevTeam2/AbstractionDemo/AbstractionPolyDemo_Acosta.java
+++ abstraction-and-polymorphism-lab/DevTeam2/AbstractionDemo/AbstractionPolyDemo_Acosta.java
@@ -19,3 +19,14 @@ public class AbstractionPolyDemo_Acosta {
         breakfast.serve();
     }
 }
+
+/* Code-Based Analysis
+* 1.) What abstract class did you create? : petAnimal_Acosta.java
+* 2.) What interface did you create? : Feed_Acosta.java
+* 3.) What methods did you override? : void prepare in KokoCrunch_Acosta from abstract class Cereal_Alvarez & void serve in Breakfast_Acosta from interface Breakfast_Alvarez
+* 4.) What methods did you overload? : void prepare(int bowlAmount) in KokoCrunch_Acosta class & void addMilk(String milkFlavor) in Breakfast_Acosta interface
+* 5.) Where does dynamic binding occur in your code? : When making the cereal object in the demo file.
+* 6.) Which part shows polymorphism? : KokoCrunch_Acosta is extended from Cereal_Alvarez class. Breakfast_Acosta implements the Breakfast_Alvarez interface.
+* 7.) How does your design achieve low coupling? : Parameters and behaviours between the classes and interfaces still work and are independent from one another.
+* 8.) How does your design achieve high cohesion? : The classes and interface have one clear responsibility.
+*/
