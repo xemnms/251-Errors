09b4491cff888e1bad1df2a2cda08654f5400d31
Clisha Rae <clishabatangan28@gmail.com>
2026-03-25 13:31:46 +0800

    Update Perfume_Batangan.java

--- abstraction-and-polymorphism-lab/DevTeam2/Abstract/Perfume_Batangan.java
+++ abstraction-and-polymorphism-lab/DevTeam2/Abstract/Perfume_Batangan.java
@@ -1,12 +1,12 @@
-//Abstract class by Batangan
-//1 abstract method and 1 concrete method
+// Abstract class by Batangan
+// 1 abstract method and 1 concrete method
 
 public abstract class Perfume_Batangan {
 
-    // abstract method
+    // Abstract method
     abstract void smell(); 
 
-    // concrete method
+    // Concrete method
     void spray(String message) {
         System.out.println("You sprayed the perfume: " + message);
     }
