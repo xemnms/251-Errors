b38a976c68bc5b1da61f1067ac628ea60557fb96
Bien Manuel P. Badosa <bienmanuelbadosa@gmail.com>
2026-03-25 13:48:13 +0000

    Badosa abspoly

--- abstraction-and-polymorphism-lab/DevTeam3/Knit_Badosa.java
+++ abstraction-and-polymorphism-lab/DevTeam3/Knit_Badosa.java
@@ -1,11 +1,27 @@
 //Interface by Badosa
-
 public interface Knit_Badosa {
-    // abstract method
-    void knit();
+    //
+    public boolean hasYarn();
 
-    // default method
-    default void displayKnit() {
-        System.out.println("Knitting in progress...");
+    public boolean hasNeedles();
+
+    default boolean canKnit() {
+        // Logic for knitting 
+        if (!hasYarn()) {
+            System.out.println("No yarn.");
+            return false;
+        }
+        if (!hasNeedles()) {
+            System.out.println("No needles.");
+            return false;
+        }
+        return true;
+    }
+    //abstract method
+    public void knit();
+
+    //default method 
+    default void prepare() {
+        System.out.println("Preparing to knit...");
     }
 }
