1084eae56c583e5d3942c4dddd70b6246c531679
Clisha Rae <clishabatangan28@gmail.com>
2026-04-09 14:53:39 +0800

    Update Calculator.java

--- exceptions-lab/Bagay_Batangan/Calculator/Calculator.java
+++ exceptions-lab/Bagay_Batangan/Calculator/Calculator.java
@@ -1,1 +1,14 @@
-//Place Holder for the Abstract Calculator Class
+public abstract class Calculator {
+
+    // DOUBLE METHODS
+    public abstract double add(double a, double b);
+    public abstract double subtract(double a, double b);
+    public abstract double multiply(double a, double b);
+    public abstract double divide(double a, double b) throws ZeroDivisionException;
+
+    // INT METHODS (OVERLOADED)
+    public abstract int add(int a, int b);
+    public abstract int subtract(int a, int b);
+    public abstract int multiply(int a, int b);
+    public abstract int divide(int a, int b) throws ZeroDivisionException;
+}
