648d725321e793721f6de1e3f66254867c5bf511
Julliana Nepomuceno <nepomucenojulliana@gmail.com>
2026-04-10 10:18:28 +0000

    Update Branch

--- exceptions-lab/DevTeam1/Alonde-Dizon-Nepomuceno/InvalidInputException_Alonde_Dizon_Nepomuceno.java
+++
@@ -1,5 +0,0 @@
-public class InvalidInputException_Alonde_Dizon_Nepomuceno extends Exception {
-    public InvalidInputException_Alonde_Dizon_Nepomuceno(String message) {
-        super(message);
-    }
-}
--- exceptions-lab/DevTeam1/Alonde-Dizon-Nepomuceno/NegativeResultException_Alonde_Dizon_Nepomuceno.java
+++
@@ -1,6 +0,0 @@
-public class NegativeResultException_Alonde_Dizon_Nepomuceno extends RuntimeException {
-    public NegativeResultException_Alonde_Dizon_Nepomuceno(String message) {
-        super(message);
-        
-    }
-}
