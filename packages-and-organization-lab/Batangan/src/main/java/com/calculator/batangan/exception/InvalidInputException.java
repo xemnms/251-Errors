c1d8eaf75735a1f9df9d71fb1e3cf44b1a130598
Clisha Rae <clishabatangan28@gmail.com>
2026-04-20 16:48:57 +0800

    Update InvalidInputException.java

--- packages-and-organization-lab/Batangan/src/main/java/com/calculator/batangan/exception/InvalidInputException.java
+++ packages-and-organization-lab/Batangan/src/main/java/com/calculator/batangan/exception/InvalidInputException.java
@@ -1,1 +1,7 @@
+package com.calculator.batangan.exception;
 
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
