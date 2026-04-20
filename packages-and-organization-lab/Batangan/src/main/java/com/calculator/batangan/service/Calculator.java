4845f91a61ede22c5d6be6d58792a39809e32705
Clisha Rae <clishabatangan28@gmail.com>
2026-04-20 16:50:40 +0800

    Update Calculator.java

--- packages-and-organization-lab/Batangan/src/main/java/com/calculator/batangan/service/Calculator.java
+++ packages-and-organization-lab/Batangan/src/main/java/com/calculator/batangan/service/Calculator.java
@@ -1,1 +1,19 @@
+package com.calculator.batangan.service;
 
+import com.calculator.batangan.exception.InvalidInputException;
+import com.calculator.batangan.exception.ZeroDivisionException;
+
+public abstract class Calculator {
+
+    // DOUBLE METHODS
+    public abstract double add(double a, double b) throws InvalidInputException;
+    public abstract double subtract(double a, double b) throws InvalidInputException;
+    public abstract double multiply(double a, double b) throws InvalidInputException;
+    public abstract double divide(double a, double b) throws InvalidInputException, ZeroDivisionException;
+
+    // INT METHODS (OVERLOADED)
+    public abstract int add(int a, int b);
+    public abstract int subtract(int a, int b);
+    public abstract int multiply(int a, int b);
+    public abstract int divide(int a, int b) throws ZeroDivisionException;
+}
