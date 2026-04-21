6d2a3476c5c4ec5dca2042b2c6cf0f0139db7860
Lynette Grace L. Galindon <galindonlynette@gmail.com>
2026-04-21 11:51:47 +0000

    galindon packages and organization lab

---
+++ packages-and-organization-lab/galindon/src/main/java/com/calculator/Galindon/exception/InvalidInputException_Galindon.java
@@ -0,0 +1,7 @@
+// Checked Exception: InvalidInputException_Arandela_Galindon.java
+package com.calculator.galindon.exception;
+public class InvalidInputException_Galindon extends Exception {
+    public InvalidInputException_Galindon(String message) {
+        super(message);
+    }
+}
