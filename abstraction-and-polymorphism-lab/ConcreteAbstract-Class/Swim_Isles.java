8d5ae342cd2fa92525a8c06617c39374e6331960
Dan Marvin M, Isles <zdanmarvin@gmail.com>
2026-04-21 17:34:37 +0800

    Create Swim_Isles.java

---
+++ abstraction-and-polymorphism-lab/ConcreteAbstract-Class/Swim_Isles.java
@@ -0,0 +1,18 @@
+/**
+ * Uses abstract class Swim_Badosa by Badosa
+ * This class was made by Isles
+ */
+
+public class Swim_Isles extends Swim_Badosa {
+
+    //override abstract method
+    @Override
+    public void swim() {
+        System.out.println("Swimming freestyle across the pool!");
+    }
+
+    //overloaded method (optional but good for demo)
+    public void swim(String style) {
+        System.out.println("Swimming using " + style + " style!");
+    }
+}
