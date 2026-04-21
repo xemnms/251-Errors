0271751a4447bd9cc3f1ca0ed5e288f944c72d30
Dan Marvin M, Isles <zdanmarvin@gmail.com>
2026-04-21 16:54:25 +0800

    Create InvalidInputException.java

---
+++ packages-and-organization-lab/isles/exception/InvalidInputException.java
@@ -0,0 +1,8 @@
+//checked exception
+package exception;
+
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
