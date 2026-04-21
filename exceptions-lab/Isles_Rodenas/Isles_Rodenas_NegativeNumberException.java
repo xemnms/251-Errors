ed31791dbf64652b7cad7dc19e8441beb8f1db89
Dan Marvin M, Isles <zdanmarvin@gmail.com>
2026-04-21 22:23:52 +0800

    Update Isles_Rodenas_NegativeNumberException.java

--- exceptions-lab/Isles_Rodenas/Isles_Rodenas_NegativeNumberException.java
+++ exceptions-lab/Isles_Rodenas/Isles_Rodenas_NegativeNumberException.java
@@ -1,4 +1,3 @@
-package exception;
 public class Isles_Rodenas_NegativeNumberException extends RuntimeException {
     public Isles_Rodenas_NegativeNumberException(String message) {
         super(message);
