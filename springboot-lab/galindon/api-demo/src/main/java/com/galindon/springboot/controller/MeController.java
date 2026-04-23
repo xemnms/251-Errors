ebe38b07804ba4288625f90c7d52ba6fef06ed93
Lynette Grace L. Galindon <galindonlynette@gmail.com>
2026-04-23 15:48:16 +0000

    galindon springboot

---
+++ springboot-lab/galindon/api-demo/src/main/java/com/galindon/springboot/controller/MeController.java
@@ -0,0 +1,20 @@
+package com.galindon.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MeController {
+
+    @GetMapping("/me")
+    public Map<String, Object> getMe() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("name", "Lynette Grace L. Galindon");
+        response.put("studentId", "2025-2022743");
+        response.put("course", "BS Computer Science");
+        response.put("message", "Learning Spring Boot REST APIs!");
+        return response;
+    }
+}
