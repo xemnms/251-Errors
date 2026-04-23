978a43b470a6c31a199a09cc9e9fd7cde1bcf7f6
Kyla Cassandra R. Rodenas <kylacassandrarodenas@gmail.com>
2026-04-23 09:35:51 +0000

    final...

--- springboot-lab/Rodenas/src/test/java/com/rodenas/springboot/MeControllerTest.java
+++ springboot-lab/Rodenas/src/test/java/com/rodenas/springboot/MeControllerTest.java
@@ -1,29 +1,22 @@
 package com.rodenas.springboot;
 
+import com.rodenas.springboot.controller.MeController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@AutoConfigureWebMvc
 public class MeControllerTest {
 
     @Autowired
     private MeController meController;
 
-    private MockMvc mockMvc = standaloneSetup(meController).build();
-
     @Test
     public void testGetMe() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/me"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.name").exists());
+        var response = meController.getMe();
+        assertThat(response.get("name")).isEqualTo("Kyla Cassandra R. Rodenas");
+        assertThat(response.get("studentId")).isEqualTo("2025-1025868");
     }
 }
