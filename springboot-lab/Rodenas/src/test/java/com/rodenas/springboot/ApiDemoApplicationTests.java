3caf5fe077524c05e3116ab217e06d7b82f858e7
Kyla Cassandra R. Rodenas <kylacassandrarodenas@gmail.com>
2026-04-23 16:29:50 +0800

    Add files via upload

---
+++ springboot-lab/Rodenas/src/test/java/com/rodenas/springboot/ApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodenas.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
