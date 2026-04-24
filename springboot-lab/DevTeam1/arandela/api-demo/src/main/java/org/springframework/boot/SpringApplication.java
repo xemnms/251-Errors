0d73d9a7b309ef7a643e51d6ab0136fb6d484400
LuxaJoy <nepomucenojulliana@gmail.com>
2026-04-24 13:43:47 +0800

    Springboot Nepomuceno and Journal

--- springboot-lab/DevTeam1/arandela/api-demo/src/main/java/org/springframework/boot/SpringApplication.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.boot;
-
-public class SpringApplication {
-    public static void run(Class<?> primarySource, String[] args) {
-        // Mock implementation for javac compilation
-    }
-}
-
--- springboot-lab/DevTeam1/nepomuceno/api-demo/src/test/java/com/nepomuceno/springboot/ApiDemoApplicationTests.java
+++ springboot-lab/DevTeam1/nepomuceno/api-demo/src/test/java/com/nepomuceno/springboot/ApiDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.arandela.springboot;
+package com.nepomuceno.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
