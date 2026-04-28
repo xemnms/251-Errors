e5e3931981075283b427f3b44939f747328ca862
Clisha Rae <clishabatangan28@gmail.com>
2026-04-28 15:00:50 +0800

    Update DatabaseDemoApplicationTests.java

--- oop-databases-lab/batangan/src/test/java/com/batangan/app/DatabaseDemoApplicationTests.java
+++ oop-databases-lab/batangan/src/test/java/com/batangan/app/DatabaseDemoApplicationTests.java
@@ -11,3 +11,16 @@ class DatabaseDemoApplicationTests {
 	}
 
 }
+/*
+What changed from previous lab?
+We changed from using hardcoded or in-memory data to storing data in a real database using JPA.
+
+Why database is better?
+Because data is saved permanently and doesn’t disappear when the app stops.
+
+How JPA helped?
+JPA made it easier to connect Java objects to the database without writing SQL manually.
+
+What changed when switching DB?
+We only changed the configuration from H2 to PostgreSQL, but the code stayed mostly the same.
+*/
