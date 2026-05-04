658308a242788b1b1812752c5125fc57bcac8afa
rmcapunpon <rmcapunpon@nu-laguna.edu.ph>
2026-05-04 02:01:47 +0000

    demo

---
+++ oop-fullstack-lab/capunpon/backend/src/main/java/com/fullstack_lab/capunpon/CapunponApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack_lab.capunpon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapunponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapunponApplication.class, args);
+	}
+
+}
---
+++ oop-fullstack-lab/capunpon/backend/src/test/java/com/fullstack_lab/capunpon/CapunponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack_lab.capunpon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapunponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
