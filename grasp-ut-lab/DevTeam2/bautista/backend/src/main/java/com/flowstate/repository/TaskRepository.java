e2b8df49efba5bd0d5210e2f0277d54508c56d3b
Lei <leibautista1417@gmail.com>
2026-05-10 07:42:15 +0000

    Delete duplicate files

--- grasp-ut-lab/DevTeam2/bautista/backend/src/main/java/com/flowstate/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flowstate.repository;
-
-import com.flowstate.entity.Task;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository
-    extends JpaRepository<Task, Long> {
-}
