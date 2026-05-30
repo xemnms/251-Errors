5fee013baeb981400eff8856936d02896bcf96b4
kayle <alondekk@students.nu-laguna.edu.ph>
2026-05-30 19:06:28 +0800

    database alonde

---
+++ oop-databases-lab/DevTeam1/alonde/database-demo/src/main/java/com/alonde/app/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.alonde.app.repository;
+
+import com.alonde.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
